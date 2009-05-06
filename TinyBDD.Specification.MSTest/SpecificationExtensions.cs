cbe6f10d67562bb10087262670da5dae495ba10d
einar@dolittle.com <einar@dolittle.com@b4d2ad24-1e26-11de-a108-cd2f117ce590>
2009-05-06 16:18:37 +0000

    Introducing MSTest flavor specifications
    git-svn-id: http://tinybdd.googlecode.com/svn/trunk@25 b4d2ad24-1e26-11de-a108-cd2f117ce590

---
+++ TinyBDD.Specification.MSTest/SpecificationExtensions.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TinyBDD.Specification.MSTest
+{
+	public static class TestExtensions
+	{
+		public static void ShouldBeEqualTo(this object valueToTest, object expected)
+		{
+			Assert.AreEqual(expected, valueToTest);
+		}
+
+		public static void ShouldNotBeEqualTo(this object valueToTest, object expected)
+		{
+			Assert.AreNotEqual(expected,valueToTest);
+		}
+
+		public static void ShouldNotBeNull(this object valueToTest)
+		{
+			Assert.IsNotNull(valueToTest);
+		}
+
+		public static void ShouldBeNull(this object valueToTest)
+		{
+			Assert.IsNull(valueToTest);
+		}
+
+		public static void ShouldBeSameAs(this object valueToTest, object expected)
+		{
+			Assert.AreSame(valueToTest, expected);
+		}
+
+		public static void ShouldNotBeSameAs(this object valueToTest, object expected)
+		{
+			Assert.AreNotSame(valueToTest, expected);
+		}
+
+		public static void ShouldBeTrue(this bool valueToTest)
+		{
+			Assert.IsTrue(valueToTest);
+		}
+
+		public static void ShouldBeFalse(this bool valueToTest)
+		{
+			Assert.IsFalse(valueToTest);
+		}
+
+		public static void ShouldBeInstanceOfType<T>(this object valueToTest)
+		{
+			Assert.IsInstanceOfType(valueToTest,typeof(T));
+		}
+
+		public static void ShouldNotBeInstanceOfType<T>(this object valueToTest)
+		{
+			Assert.IsNotInstanceOfType(valueToTest, typeof(T));
+		}
+
+		public static void ShouldBeEmpty(this IList list)
+		{
+			Assert.AreEqual(0,list.Count);
+		}
+
+		public static void ShouldContain(this IList list, object expectedObject)
+		{
+			var index = list.IndexOf(expectedObject);
+			if( index == -1 )
+			{
+				Assert.Fail("Expected object was not in list");
+			}
+		}
+
+		public static void ShouldHave(this IList list, int expectedCount)
+		{
+			Assert.AreEqual(expectedCount,list.Count);
+		}
+
+		public static void ShouldThrowException<T>(this object anObj, Action action, Action<T> exception) where T : Exception
+		{
+			Exception ex = null;
+
+			try
+			{
+				action.Invoke();
+			}
+			catch (Exception e)
+			{
+				ex = e;
+			}
+
+			if (ex == null)
+				Assert.Fail("Did not throw exception");
+			else
+			{
+				if (ex.GetType() != typeof(T))
+				{
+					Assert.Fail("Did not throw expected exception ({0}), but {1}");
+				}
+				else
+				{
+					exception.Invoke((T) ex);
+				}
+			}
+
+		}
+	}
+}
