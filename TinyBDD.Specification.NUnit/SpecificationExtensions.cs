fa6d1256a234fffc01019fb86b0ce3edd943795a
goeran.hansen <goeran.hansen@b4d2ad24-1e26-11de-a108-cd2f117ce590>
2009-05-06 18:58:25 +0000

    Copied inn extension methods from MSTest Specification
    git-svn-id: http://tinybdd.googlecode.com/svn/trunk@28 b4d2ad24-1e26-11de-a108-cd2f117ce590

--- TinyBDD.Specification.NUnit/SpecificationExtensions.cs
+++ TinyBDD.Specification.NUnit/SpecificationExtensions.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using NUnit.Framework;
 using TinyBDD.Specification.Templates;
+using System.Collections;
 
 namespace TinyBDD.Specification.NUnit
 {
@@ -14,9 +15,68 @@ namespace TinyBDD.Specification.NUnit
             Assert.AreEqual(value, anObj);
         }
 
+        public static void ShoultNotEqual(this object valueToTest, object expected)
+        {
+            Assert.AreNotEqual(expected, valueToTest);
+        }
+
         public static void ShouldNotBeNull(this object anObj)
         {
             Assert.IsNotNull(anObj);
+        }
+
+        public static void ShouldBeNull(this object valueToTest)
+        {
+            Assert.IsNull(valueToTest);
+        }
+
+        public static void ShouldBeSameAs(this object valueToTest, object expected)
+        {
+            Assert.AreSame(valueToTest, expected);
+        }
+
+        public static void ShouldNotBeSameAs(this object valueToTest, object expected)
+        {
+            Assert.AreNotSame(valueToTest, expected);
+        }
+
+        public static void ShouldBeTrue(this bool valueToTest)
+        {
+            Assert.IsTrue(valueToTest);
+        }
+
+        public static void ShouldBeFalse(this bool valueToTest)
+        {
+            Assert.IsFalse(valueToTest);
+        }
+
+        public static void ShouldBeInstanceOfType<T>(this object valueToTest)
+        {
+            Assert.IsInstanceOfType(typeof(T), valueToTest);
+        }
+
+        public static void ShouldNotBeInstanceOfType<T>(this object valueToTest)
+        {
+            Assert.IsNotInstanceOfType(typeof(T), valueToTest);
+        }
+
+        public static void ShouldBeEmpty(this IList list)
+        {
+            Assert.AreEqual(0, list.Count);
+        }
+
+        public static void ShouldContain(this IList list, object expectedObject)
+        {
+            var index = list.IndexOf(expectedObject);
+            if (index == -1)
+            {
+                Assert.Fail("Expected object was not in list");
+            }
+        }
+
+        public static void ShouldHave(this IList list, int expectedCount)
+        {
+            Assert.AreEqual(expectedCount, list.Count);
         }
 
         public static void ShouldThrowException<T>(this object anObj, Action action, Action<T> exception) where T : Exception
@@ -24,6 +84,7 @@ namespace TinyBDD.Specification.NUnit
             ShouldThrowExceptionTemplate<T> template = new ShouldThrowExceptionTemplate<T>(
                 () => action.Invoke(), m => Assert.Fail(m), exception);
             template.Execute();
+
         }
     }
 }
