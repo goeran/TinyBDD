85c2d61f796f487e359ab9b4cbfb9e719b883170
goeran.hansen <goeran.hansen@b4d2ad24-1e26-11de-a108-cd2f117ce590>
2009-05-30 12:42:19 +0000

    forgot some test files
    git-svn-id: http://tinybdd.googlecode.com/svn/trunk@42 b4d2ad24-1e26-11de-a108-cd2f117ce590

---
+++ TinyBDDTests/Learning/CreateCodeAtRuntimeTests.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+using System.Reflection.Emit;
+using System.Reflection;
+
+namespace TinyBDDTests.Learning
+{
+    [TestFixture]
+    public class CreateCodeAtRuntimeTests
+    {
+        public void Haldis()
+        {
+            Console.WriteLine("Hello world!");
+        }
+
+        [Test]
+        public void Create_class()
+        {
+            ILGenerator ilGenerator;
+            AssemblyBuilder asmBuilder = AppDomain.CurrentDomain.DefineDynamicAssembly(
+                new AssemblyName("Scenarios"), AssemblyBuilderAccess.RunAndSave);
+
+            ModuleBuilder moduleBuilder = asmBuilder.DefineDynamicModule("Scenarios", "Scenarios.dll");
+
+            var typeBuilder = moduleBuilder.DefineType("Scenario_Checkout", TypeAttributes.Class | TypeAttributes.Public);
+            var attrBuilder = new CustomAttributeBuilder(typeof(TestFixtureAttribute).GetConstructor(new Type[]{ }), new Object[]{ });
+            typeBuilder.SetCustomAttribute(attrBuilder);
+
+            var method = typeBuilder.DefineMethod("ShouldGetLatestChangeset", MethodAttributes.Public);
+            attrBuilder = new CustomAttributeBuilder(typeof(TestAttribute).GetConstructor(new Type[]{ }), new Object[]{ });
+            method.SetCustomAttribute(attrBuilder);
+            ilGenerator = method.GetILGenerator();
+            ilGenerator.Emit(OpCodes.Ldstr, "Hello world");
+            ilGenerator.Emit(OpCodes.Call,
+                typeof(Console).GetMethod("WriteLine", 
+                    new Type[] { typeof(string)}));
+            //ilGenerator.Emit(OpCodes.Ldc_I4_0);
+            ilGenerator.Emit(OpCodes.Ret);
+            /*
+            var thisType = typeof(CreateCodeAtRuntimeTests);
+            var methodBody = thisType.GetMethod("Haldis").GetMethodBody().GetILAsByteArray();
+            method.CreateMethodBody(methodBody, methodBody.Length);
+            */
+            typeBuilder.CreateType();
+
+            asmBuilder.Save("Scenarios.dll");
+        }
+    }
+}
---
+++ TinyBDDTests/Learning/FileSystemWatcherTests.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+using System.IO;
+using System.Threading;
+using TinyBDD.Specification.NUnit;
+
+namespace TinyBDDTests.Learning
+{
+    [TestFixture]
+    public class FileSystemWatcherTests
+    {
+        FileSystemWatcher fsWatcher;
+        string directoryToMonitor = Environment.CurrentDirectory;
+
+        [SetUp]
+        public void Setup()
+        {
+            fsWatcher = new FileSystemWatcher(directoryToMonitor);
+            fsWatcher.EnableRaisingEvents = true;
+
+            foreach (var file in Directory.GetFiles(directoryToMonitor))
+                if (file.EndsWith(".test"))
+                    File.Delete(file);
+        }
+
+        [Test]
+        public void Should_trigger_notify_when_file_created()
+        {
+            bool notified = false;
+
+            fsWatcher.Created += (o, e) =>
+            {
+                notified = true;
+            };
+
+            File.Create(
+                Path.Combine(directoryToMonitor, "goeran.test"));
+
+            Thread.Sleep(50);
+
+            notified.ShouldBeTrue();
+
+        }
+    }
+}
