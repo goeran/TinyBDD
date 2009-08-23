bcd267b93fdde852c1912bb24a55df3c5488dcb4
goeran.hansen <goeran.hansen@b4d2ad24-1e26-11de-a108-cd2f117ce590>
2009-08-23 17:20:30 +0000

    Working on extending the Scenario dsl introducing the base class Behaviour.
    git-svn-id: http://tinybdd.googlecode.com/svn/trunk@52 b4d2ad24-1e26-11de-a108-cd2f117ce590

---
+++ TinyBDD/Dsl/GivenWhenThen/Behaviour.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using TinyBDD.SemanticModel;
+
+namespace TinyBDD.Dsl.GivenWhenThen
+{
+    public class Behaviour<TClass>
+    {
+        private AAA semanticModel;
+        private Semantics scenario;
+
+        public Behaviour()
+        {
+            
+        }
+
+        public Behaviour(AAA semanticModel)
+        {
+            this.semanticModel = semanticModel;
+            scenario = new Semantics(Activator.CreateInstance(typeof(TClass)), semanticModel);
+        }
+
+        public void Given(string text)
+        {
+            scenario.Given(text);
+        }
+
+        public void Given(string text, Action action)
+        {
+            scenario.Given(text, action);
+        }
+
+        public void Given(Context context)
+        {
+            scenario.Given(context);
+        }
+
+        public void When(string text)
+        {
+            scenario.When(text);
+        }
+
+        public void When(string text, Action action)
+        {
+            scenario.When(text, action);
+        }
+
+        public void When(When when)
+        {
+            scenario.When(when);
+        }
+
+        public void Then(string text)
+        {
+            scenario.Then(text);
+        }
+
+        public void Then(string text, Action action)
+        {
+            scenario.Then(text, action);
+        }
+    }
+}
---
+++ TinyBDDTests/Dsl/GivenWhenThen/BehaviourSpecs.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+using TinyBDD.Dsl.GivenWhenThen;
+using TinyBDD.SemanticModel;
+using TinyBDD.Specification.NUnit;
+using Assert=NUnit.Framework.Assert;
+
+namespace TinyBDDTests.Dsl.GivenWhenThen.BehaviourSpecs
+{
+    public class Changeset_notification : Behaviour<Changeset_notification>
+    {
+        public static Context there_are_changesets_in_SourceControl = () => { };
+        public static When notified_to_refresh = () => { };
+
+        public Changeset_notification()
+        {
+            
+        }
+
+        public Changeset_notification(AAA semanticModel)
+            : base(semanticModel)
+        {
+            
+        }
+    }
+
+    [TestFixture]
+    public class When_describing_scenario
+    {
+        private AAA semanticModel;
+        private AAAMemento semanticModelState;
+        private Behaviour<Changeset_notification> scenario;
+
+        [SetUp]
+        public void Setup()
+        {
+            semanticModelState = new AAAMemento();
+            semanticModel = new AAA(semanticModelState);
+            scenario = new Changeset_notification(semanticModel);
+        }
+
+        [Test]
+        public void Assure_Given_add_arrange_to_the_SemanticModel()
+        {
+            scenario.Given("there are changesets in SourceControl");
+            AssertArrange("there are changesets in SourceControl");
+            semanticModelState.Arranges.Clear();
+
+            scenario.Given("there are changesets in SourceControl", () => { });
+            AssertArrange("there are changesets in SourceControl");
+            semanticModelState.Arranges.Clear();
+
+            scenario.Given(Changeset_notification.there_are_changesets_in_SourceControl);
+            AssertArrange("there are changesets in SourceControl");
+            semanticModelState.Arranges.Clear();
+        }
+
+        private void AssertArrange(string text)
+        {
+            semanticModelState.Arranges.ShouldHave(1);
+            semanticModelState.Arranges.First().Text.ShouldBe(text);
+        }
+
+        [Test]
+        public void Assure_When_add_act_to_the_SemanticModel()
+        {
+            scenario.When("notified to refresh");
+            AssertAct("notified to refresh");
+            semanticModelState.Acts.Clear();
+
+            scenario.When("notified to refresh", () => { });
+            AssertAct("notified to refresh");
+            semanticModelState.Acts.Clear();
+
+            scenario.When(Changeset_notification.notified_to_refresh);
+            AssertAct("notified to refresh");
+            semanticModelState.Acts.Clear();
+        }
+
+        private void AssertAct(string text)
+        {
+            semanticModelState.Acts.Count.ShouldBe(1);
+            semanticModelState.Acts.Keys.First().Text.ShouldBe(text);
+        }
+
+        [Test]
+        public void Assure_Then_add_Assert_to_the_SemanticModel()
+        {
+            //The SemanticModel expect and act, before an
+            //assert can take place.
+            scenario.When("notified to refresh");
+
+            scenario.Then("all changesets are received");
+            VerifyAssertBeenAdded("all changesets are received");
+            semanticModelState.Acts.Clear();
+
+            scenario.When("notified to refresh");
+            scenario.Then("all changesets are received", () => { });
+            VerifyAssertBeenAdded("all changesets are received");
+            semanticModelState.Acts.Clear();
+        }
+
+        private void VerifyAssertBeenAdded(string text)
+        {
+            semanticModelState.Acts.Values.First().ShouldHave(1);
+            semanticModelState.Acts.Values.First().First().Text.ShouldBe(text);
+        }
+
+    }
+}
