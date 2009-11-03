6cf0da9b7e00c11759a713d56880d4aa05270e9f
goeran.hansen <goeran.hansen@b4d2ad24-1e26-11de-a108-cd2f117ce590>
2009-11-03 22:28:09 +0000

    Started on unit tests for the specifications
    git-svn-id: http://tinybdd.googlecode.com/svn/trunk@64 b4d2ad24-1e26-11de-a108-cd2f117ce590

---
+++ TinyBDD.Specification.NUnitTests/ShouldThrowExceptionSpecs.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+
+using TinyBDD.Specification.NUnit;
+
+namespace TinyBDD.SpecificationTests.NUnit.SpecificationExtensionsSpecs
+{
+    [TestFixture]
+    public class ShouldThrowExceptionSpecs
+    {
+        [Test]
+        public void Should_throw_AssertionException_if_specified_exception_is_not_thrown()
+        {
+            bool exceptionThrown = false;
+            try
+            {
+                this.ShouldThrowException<Exception>(() => { }, ex => { });
+            }
+            catch (AssertionException ex)
+            {
+                exceptionThrown = true;
+            }
+
+            Assert.IsTrue(exceptionThrown, "AssertionException was not thrown");
+        }
+
+        [Test]
+        public void Should_not_throw_AssertionException_if_specified_exception_is_thrown()
+        {
+            bool exceptionThrown = false;
+
+            try
+            {
+                this.ShouldThrowException<ArgumentNullException>(() =>
+                {
+                    throw new ArgumentNullException();
+                }, ex => {});
+            }
+            catch (AssertionException)
+            {
+                exceptionThrown = true;
+            }
+
+            Assert.IsFalse(exceptionThrown, "AssertionException was not thrown");
+        }
+    }
+}
