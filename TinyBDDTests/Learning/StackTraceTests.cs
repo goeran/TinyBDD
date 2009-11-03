cf2f32a177ed30365b77f053a875cb1bd820674d
goeran.hansen <goeran.hansen@b4d2ad24-1e26-11de-a108-cd2f117ce590>
2009-11-03 23:09:15 +0000

    Wrote a learning test to find out how to get the callers method name. Easy using StackeTrace
    git-svn-id: http://tinybdd.googlecode.com/svn/trunk@70 b4d2ad24-1e26-11de-a108-cd2f117ce590

---
+++ TinyBDDTests/Learning/StackTraceTests.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+using System.Diagnostics;
+
+using TinyBDD.Specification.NUnit;
+
+namespace TinyBDDTests.Learning
+{
+    [TestFixture]
+    public class StackTraceTests
+    {
+        [Test]
+        public void Get_callers_method_name()
+        {
+            StackTrace a = new StackTrace();
+            var frames = a.GetFrames();
+            var parentMethod = frames.First().GetMethod();
+            Console.WriteLine(parentMethod.Name);
+            parentMethod.Name.ShouldBe("Get_callers_method_name");
+        }
+    }
+}
