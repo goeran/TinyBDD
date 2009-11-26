100195298cb8f515deb19d3147eaf75f86b762c1
goeran.hansen <goeran.hansen@b4d2ad24-1e26-11de-a108-cd2f117ce590>
2009-11-26 20:13:49 +0000

    added special handling for ShouldContain and string lists because of a wired bug in Smeedee
    git-svn-id: http://tinybdd.googlecode.com/svn/trunk@86 b4d2ad24-1e26-11de-a108-cd2f117ce590

---
+++ TinyBDD.Specification.NUnitTests/ShouldContainSpecs.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+using TinyBDD.Specification.NUnit;
+
+namespace TinyBDD.Specification.NUnitTests
+{
+    [TestFixture]
+    public class ShouldContainSpecs
+    {
+        private List<string> list;
+
+        [SetUp]
+        public void Setup()
+        {
+            list = new List<string>();
+            list.Add("hello");
+            list.Add("world");
+        }
+
+        [Test]
+        public void Should_not_throw_AssertException_when_item_is_in_list()
+        {
+            list.ShouldContain("hello");
+        }
+
+        [Test]
+        [ExpectedException(typeof(AssertionException))]
+        public void Should_throw_AssertException_when_item_is_not_in_list()
+        {
+            list.ShouldContain("not in list");
+        }
+    }
+}
