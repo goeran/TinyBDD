a3b59e8e0e9c01f541d198069ad829feeed0b18b
goeran.hansen <goeran.hansen@b4d2ad24-1e26-11de-a108-cd2f117ce590>
2010-03-28 17:24:40 +0000

    Forgot the project file
    git-svn-id: http://tinybdd.googlecode.com/svn/trunk@91 b4d2ad24-1e26-11de-a108-cd2f117ce590

---
+++ TinyBDD.Specification.NUnit.2.5.3/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("TinyBDD.Specification.NUnit.2.5.3")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("eXPerience")]
+[assembly: AssemblyProduct("TinyBDD.Specification.NUnit.2.5.3")]
+[assembly: AssemblyCopyright("Copyright © eXPerience 2010")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("47dd52c9-99ef-4a12-af60-229c9999a7e2")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("0.1.0.1")]
+[assembly: AssemblyFileVersion("0.1.0.1")]
