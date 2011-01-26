aba3a5e99a1fbad170c6673814e9739490decb49
unknown <gohans@.statoil.net>
2011-01-26 14:56:31 +0100

    git didn't handle norwegian chars in filename, so we had to rename a file

---
+++ TinyBDD/Dsl/GivenWhenThenNO/SaaSemantikk.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using TinyBDD.SemanticModel;
+using TinyBDD.Dsl.GivenWhenThen;
+
+namespace TinyBDD.Dsl.GivenWhenThenNO
+{
+    public class SåSemantikk
+    {
+        AAA semantiskModell;
+        TestMetadataParser metadataParser;
+        object test;
+
+        public SåSemantikk(AAA semantiskModell, object test)
+        {
+            this.test = test;
+            this.semantiskModell = semantiskModell;
+            this.metadataParser = new TestMetadataParser(test);
+        }
+
+        public SåSemantikk Og(Så kode)
+        {
+            return Og(metadataParser.TranslateToText(kode), () => { kode(); });
+        }
+
+        public SåSemantikk Og(string tekstligBeskrivelse)
+        {
+            return Og(tekstligBeskrivelse, () => { });
+        }
+
+        public SåSemantikk Og(string tekstligBeskrivelse, Action kode)
+        {
+            semantiskModell.Assert(tekstligBeskrivelse, kode);
+
+            return new SåSemantikk(semantiskModell, test);
+        }
+    }
+}
